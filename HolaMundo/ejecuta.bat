1b81887ffa5ca3b0ee032b2c41bb9fea754632b9
Fatake <pace_lozano@hotmail.com>
2021-01-17 14:33:02 -0600

    Cargando hola mundo

---
+++ HolaMundo/ejecuta.bat
@@ -0,0 +1,2 @@
+go run variables.go
+pause
---
+++ HolaMundo/variables.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var entero int    //inicia en 0
+	var cadena string //inicia en vacio
+	var bandera bool  //inicia en fallse
+	algo := "variable defida en asignacion"
+	//var arreglo []int
+	fmt.Printf("Entero %d\n", entero)
+	fmt.Printf("cadena %s\n", cadena)
+	fmt.Printf("bandera %t\n", bandera)
+	fmt.Printf("hello, world %s\n", algo)
+}
