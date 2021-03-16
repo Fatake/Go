2b909663420d5dfdeb1efc2ee40cccb1c1a3051e
Fatake <pace_lozano@hotmail.com>
2021-03-16 11:50:33 -0600

    Agregando defer

--- Archivos/file2.go
+++ Archivos/file2.go
@@ -11,6 +11,13 @@ func main() {
 	fmt.Println("Lectura de Linea a Linea\n<------------------------->")
 
 	file, err := os.Open("./hola.txt")
+
+	// stack de funciones que se ejecuta cuando la funcion actual retorna
+	defer func() {
+		file.Close()
+		fmt.Println("Cerrando el archivo")
+	}()
+
 	if err != nil {
 		os.Exit(1)
 	}
@@ -22,6 +29,4 @@ func main() {
 		fmt.Println(linea)
 		time.Sleep(250 * time.Millisecond)
 	}
-
-	file.Close()
 }
