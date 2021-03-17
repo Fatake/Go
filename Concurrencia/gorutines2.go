79909fc8f46ed7266f834c55e8c58fdcd6905945
Fatake <pace_lozano@hotmail.com>
2021-03-16 18:47:05 -0600

    Espera de rutinas

---
+++ Concurrencia/gorutines2.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+// wg es utilizado para indicar al programa que espere a
+// que finalicen las rutinas
+var wg sync.WaitGroup
+
+func main() {
+	wg.Add(2) // indicamos que esperara dos rutinas
+	fmt.Println("[m] Iniciando go rutines")
+
+	go imprimirCantidad("A")
+
+	go imprimirCantidad("B")
+
+	fmt.Println("[m] Esperando rutinas...")
+	wg.Wait()
+	fmt.Println("\n[m] end")
+
+}
+
+func imprimirCantidad(etiqueta string) {
+	defer wg.Done() // indicamos cuando ya termina la go rutine
+	for i := 0; i < 10; i++ {
+		sleep := rand.Int63n(1000)
+		time.Sleep(time.Duration(sleep) * time.Millisecond)
+		fmt.Printf("[%s] Trabajando %d\n", etiqueta, (i + 1))
+	}
+}
