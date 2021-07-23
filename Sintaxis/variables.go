4eed28f096a41976207cb3ad223c903e1bcd0d07
Fatake <pace_lozano@hotmail.com>
2021-07-23 14:33:30 -0500

    AGREGANDO COMPLEJOS

--- Sintaxis/variables.go
+++ Sintaxis/variables.go
@@ -10,6 +10,7 @@ func main() {
 	var entero int    //inicia en 0
 	var cadena string //inicia en vacio
 	var bandera bool  //inicia en fallse
+	var complejo complex64 = 10 + 8i
 	algo := "variable defida en asignacion"
 
 	//var arreglo []int
@@ -17,6 +18,7 @@ func main() {
 	fmt.Printf("cadena %s\n", cadena)
 	fmt.Printf("bandera %t\n", bandera)
 	fmt.Printf("hello, world %s\n", algo)
+	fmt.Println("Complejo ", complejo)
 	fmt.Println(mensaje)
 	fmt.Println(easyMessage)
 
