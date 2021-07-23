0ee0bca4bbca74beefe4e621dba27e6cd63d19cb
Fatake <pace_lozano@hotmail.com>
2021-07-23 14:25:40 -0500

    variables y funciones go

--- Sintaxis/funciones.go
+++ Sintaxis/funciones.go
@@ -4,30 +4,7 @@ package main //package name
 import "fmt" // librerías necesarias para importar el paquete
 
 func main() {
-	// Variables
-	var mensaje string = "Hola mundo"
-	easyMessage := "Hola mundo usando :="
-	fmt.Println(mensaje)
-	fmt.Println(easyMessage)
-
-	// Puedes comentar usando "//"
-
-	// float numbers
-	a := 10.
-	var b float64 = 3
-	fmt.Println(a / b)
-
-	//integer numbers
-	var c int = 10
-	d := 3
-	fmt.Println(c / d)
-
-	// boolean
-	var x bool = true
-	y := false
-	fmt.Println(x || y)
-	fmt.Println(x && y)
-	fmt.Println(!x)
+	fmt.Println("Tipos de funciones en Go, Publicas, privada y defer")
 
 	//Lógica privada
 	privada()
--- Sintaxis/variables.go
+++ Sintaxis/variables.go
@@ -1,15 +1,42 @@
-package main
+package main //package name
+// package	otroPaquete
 
-import "fmt"
+import "fmt" // librerías necesarias para importar el paquete
 
 func main() {
+	// Variables
+	var mensaje string = "Hola mundo"
+	easyMessage := "Hola mundo usando :="
 	var entero int    //inicia en 0
 	var cadena string //inicia en vacio
 	var bandera bool  //inicia en fallse
 	algo := "variable defida en asignacion"
+
 	//var arreglo []int
 	fmt.Printf("Entero %d\n", entero)
 	fmt.Printf("cadena %s\n", cadena)
 	fmt.Printf("bandera %t\n", bandera)
 	fmt.Printf("hello, world %s\n", algo)
+	fmt.Println(mensaje)
+	fmt.Println(easyMessage)
+
+	// Puedes comentar usando "//"
+
+	// float64 bits numbers
+	a := 10.
+	var b float64 = 3
+	fmt.Println(a / b)
+
+	//integer numbers
+	var c int = 10
+	d := 3
+	fmt.Println(c / d)
+
+	// boolean
+	var x bool = true
+	y := false
+	fmt.Println(x || y)
+	fmt.Println(x && y)
+	fmt.Println(!x)
+
 }
