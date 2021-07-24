1accd37b55c938e2562fa39a0a63c79e88f8775a
Fatake <pace_lozano@hotmail.com>
2021-07-24 13:55:28 -0500

    Punteros

--- Sintaxis/Punteros/main.go
+++ Sintaxis/Punteros/main.go
@@ -14,3 +14,30 @@ func main() {
 	myPc.DuplicateRAM()
 	myPc.FormatPrint()
 }
+
+type Pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+func New(rm, dsk int, brn string) Pc {
+	myPc := Pc{ram: rm, disk: dsk, brand: brn}
+	return myPc
+}
+
+func (self Pc) FormatPrint() {
+	fmt.Printf("Esta pc marca %s cuenta con una ram de %dGB y un disco de %dGB.\n", self.brand, myPc.ram, myPc.disk)
+}
+
+func (myPc *Pc) DuplicateRAM() {
+	myPc.ram = myPc.ram * 2
+}
+
+func (myPc Pc) GetRam() int {
+	return myPc.ram
+}
+
+func (myPc *Pc) SetRam(rm int) {
+	myPc.ram = rm
+}
